73293311203bf84be90660896b70c82c61758222
santhoshrampam <santhoshrampam999@gmail.com>
2025-08-08 00:34:49 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         Board board = new Board();
-        board.printBoard();
+        board.printBoard(); // This will display your chess board
     }
 }

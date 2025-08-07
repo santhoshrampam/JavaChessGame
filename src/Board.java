094c6637374e1053c4f8d8d19e5d01db16d23759
santhoshrampam <santhoshrampam999@gmail.com>
2025-08-08 00:37:42 +0530

    Update Board.java

--- src/Board.java
+++ src/Board.java
@@ -28,3 +28,12 @@ public class Board {
         }
     }
 }
+    public void printBoard() {
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+

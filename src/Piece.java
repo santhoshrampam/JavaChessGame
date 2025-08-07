44e324634691080f850da44ee7826cc8effaee90
santhoshrampam <santhoshrampam999@gmail.com>
2025-08-08 00:24:57 +0530

    Create Piece.java

---
+++ src/Piece.java
@@ -0,0 +1,34 @@
+public class Piece {
+    private String name;
+    private String color;
+
+    public Piece(String name, String color) {
+        this.name = name;
+        this.color = color;
+    }
+
+    public String getSymbol() {
+        switch (name.toLowerCase()) {
+            case "king":
+                return color.equalsIgnoreCase("white") ? "♔" : "♚";
+            case "queen":
+                return color.equalsIgnoreCase("white") ? "♕" : "♛";
+            case "rook":
+                return color.equalsIgnoreCase("white") ? "♖" : "♜";
+            case "bishop":
+                return color.equalsIgnoreCase("white") ? "♗" : "♝";
+            case "knight":
+                return color.equalsIgnoreCase("white") ? "♘" : "♞";
+            case "pawn":
+                return color.equalsIgnoreCase("white") ? "♙" : "♟";
+            default:
+                return ".";
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return

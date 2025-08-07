186d986d5680bd3d61c22afb03f57f33496e7b6d
santhoshrampam <santhoshrampam999@gmail.com>
2025-08-08 00:19:17 +0530

    Update Main.java

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to Java Chess Game!");
+        Board board = new Board();
+        board.printBoard();
     }
 }
-
